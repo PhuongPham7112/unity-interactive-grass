acd423f3736e05e2894f69f4bccccd9e2dd08370
PhuongPham7112 <phuongp@usc.edu>
2024-09-27 20:58:42 -0700

    upload colliders info

--- Assets/Scripts/GrassModel.cs
+++ Assets/Scripts/GrassModel.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class GrassModel : MonoBehaviour
 {
     private int kernelIndex;
+    private Vector4[] collidersData; // colliders position + extent
     private Vector4[] grassV1Positions; // v1 xyz + grass height
     private Vector4[] grassV2Positions; // v2 xyz + grass width
     private Vector3[] grassGroundPositions; // v2 xyz + grass width
     
+    private ComputeBuffer collidersBuffer;
     private ComputeBuffer groundPosBuffer;
     private ComputeBuffer grass1PosBuffer;
     private ComputeBuffer grass2PosBuffer;
-    private ComputeBuffer grassLength;
-    private MaterialPropertyBlock propertyBlock;
     
     [SerializeField] public SphereCollider[] colliders;
     [SerializeField] private ComputeShader grassPhysicsCS;
@@ -26,6 +26,9 @@ public class GrassModel : MonoBehaviour
     [SerializeField] public float grassWidth = 1.0f;
     [SerializeField] public float grassHeight = 1.0f;
     [SerializeField] private int numPoints;
+    [SerializeField] private int numColliders;
+
+    private MaterialPropertyBlock propertyBlock;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +38,7 @@ public class GrassModel : MonoBehaviour
 
         // Get number of child objects
         numPoints = gameObject.transform.childCount;
+        numColliders = colliders.Length;
 
         // Fill the buffer with the v2 positions of the child objects
         grassGroundPositions = new Vector3[numPoints];
@@ -47,17 +51,29 @@ public class GrassModel : MonoBehaviour
             grassV2Positions[i] = new Vector4(0, grassHeight, Mathf.Epsilon, grassWidth);
         }
 
+
+        collidersData = new Vector4[numColliders];
+        for (int i = 0; i < numColliders; i++)
+        {
+            collidersData[i] = new Vector4(colliders[i].center.x, 
+                colliders[i].center.y,
+                colliders[i].center.z,
+                colliders[i].radius);
+            Debug.Log(colliders[i].radius);
+        }
+
         // Setup buffers
+        collidersBuffer = new ComputeBuffer(numColliders, sizeof(float) * 4);
         groundPosBuffer = new ComputeBuffer(numPoints, sizeof(float) * 3);
-        grassLength = new ComputeBuffer(numPoints, sizeof(float));
         grass1PosBuffer = new ComputeBuffer(numPoints, sizeof(float) * 4);
         grass2PosBuffer = new ComputeBuffer(numPoints, sizeof(float) * 4);
 
+        collidersBuffer.SetData(collidersData);
         groundPosBuffer.SetData(grassGroundPositions);
         grass1PosBuffer.SetData(grassV1Positions);
         grass2PosBuffer.SetData(grassV2Positions);
 
-        grassPhysicsCS.SetBuffer(kernelIndex, "grassLength", grassLength);
+        grassPhysicsCS.SetBuffer(kernelIndex, "colliders", collidersBuffer);
         grassPhysicsCS.SetBuffer(kernelIndex, "v1Positions", grass1PosBuffer);
         grassPhysicsCS.SetBuffer(kernelIndex, "v2Positions", grass2PosBuffer);
         grassPhysicsCS.SetBuffer(kernelIndex, "groundPositions", groundPosBuffer);
@@ -94,25 +110,11 @@ public class GrassModel : MonoBehaviour
                 childRenderer.SetPropertyBlock(propertyBlock);
             }
         }
-
-        // check
-        //UnityEngine.Rendering.AsyncGPUReadback.Request(grassLength, request =>
-        //{
-        //    if (request.hasError)
-        //    {
-        //        Debug.Log("GPU readback error detected.");
-        //        return;
-        //    }
-        //    float result = request.GetData<float>()[0];
-        //    // Use the result here
-        //    Debug.Log(result);
-        //});
-
     }
 
     void OnDestroy()
     {
-        grassLength?.Release();
+        collidersBuffer?.Release();
         groundPosBuffer?.Release();
         grass1PosBuffer?.Release();
         grass2PosBuffer?.Release();
